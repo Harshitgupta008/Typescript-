db2139b60cf460157a6c4c80d76e911edbea829a
harshitgupta008 <harshitguptas0408@gmail.com>
2024-12-29 00:11:58 +0530

    update

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var username = { name: "harshit", password: 345 };
+
 console.log(username.password);
+
+var countArr = function (numbers) {
+    return numbers.reduce(function (acc, num) {
+        return acc + num;
+    }, 0);
+};
+console.log(countArr([2, 3, 4, 5]));
--- app.ts
+++ app.ts
@@ -3,6 +3,16 @@ interface User {
     password: number;
 }
 
-const username:User = { name: "harshit", password: 345 };
+const username: User = { name: "harshit", password: 345 };
 
-console.log(username.password); 
+console.log(username.password);
+
+const countArr = (numbers:number[]) => {
+
+    return numbers.reduce((acc, num) => {
+        return acc + num;
+    }, 0);
+
+}
+
+console.log(countArr([2,3,4,5]));
